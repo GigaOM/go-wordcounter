8e2a67bfe155ac78417b7492d04ed67c88863cb5
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:22:51 -0400

    Update go-wordcounter.php

--- go-wordcounter.php
+++ go-wordcounter.php
@@ -4,6 +4,8 @@
  * Description:	Adds server-side word counter functionality to WordPress.
  * Author:			Gigaom Network
  * Author URI:		http://gigaomnetwork.com/
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-wordcounter.php';
